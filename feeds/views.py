9fb414f62f6da026c1e33c272ee2c138e726858c
Emanuele Rocca <ema@linux.it>
2010-05-27 14:11:31 +0200

    Basic feed rendering

--- feeds/views.py
+++ feeds/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 
 from feeds.models import get_user, get_user_feeds, get_feed
 
+import feedparser
+
 import logging
 
 def user_feeds(self, username):
@@ -17,4 +19,11 @@ def user_feeds(self, username):
 
 def render_feed(self, username, feed_key):
     feed = get_feed(feed_key)
+
+    entries = []
+    channels = feedparser.parse(feed.url)
+    for entry in channels.entries:
+        entries.append(dict(title=entry.title,
+            description=entry.description, link=entry.link))
+
     return render_to_response("render_feed.html", locals())
