74cae2dc2e89fff2db78b6d4ea085db7d295b448
Emanuele Rocca <ema@linux.it>
2010-05-27 15:53:55 +0200

    addfeed initial implementation

--- feeds/views.py
+++ feeds/views.py
@@ -3,12 +3,13 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 
 from feeds.models import get_user, get_user_feeds, get_feed
+from feeds.models import add_user_subscription
 
 import feedparser
 
 import logging
 
-def user_feeds(self, username):
+def user_feeds(request, username):
     logging.info("user_feeds(%s)" % username)
 
     if not get_user(username):
@@ -17,7 +18,7 @@ def user_feeds(self, username):
     user_feeds = get_user_feeds(username)
     return render_to_response("list_feeds.html", locals())
 
-def render_feed(self, username, feed_key):
+def render_feed(request, username, feed_key):
     feed = get_feed(feed_key)
 
     entries = []
@@ -27,3 +28,13 @@ def render_feed(self, username, feed_key):
             description=entry.description, link=entry.link))
 
     return render_to_response("render_feed.html", locals())
+
+def add_feed(request, username):
+    rssurl = request.POST['rssurl']
+
+    try:
+        add_user_subscription(username, rssurl)
+    except KeyError:
+        return HttpResponse("Cannot parse feed: %s" % rssurl)
+
+    return HttpResponseRedirect('/%s/' % username)
