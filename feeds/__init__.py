449a574bf6ef8795ff58ea2188edeccbdb210fa3
Emanuele Rocca <ema@linux.it>
2010-05-28 17:59:54 +0200

    Avoid loading initial data, display username in list_feeds

--- feeds/__init__.py
+++ feeds/__init__.py
@@ -1,5 +1,5 @@
 from models import User
 from feeds.models import insert_users_and_subscriptions
 
-if User.all().count() == 0:
-    insert_users_and_subscriptions()
+#if User.all().count() == 0:
+#    insert_users_and_subscriptions()
